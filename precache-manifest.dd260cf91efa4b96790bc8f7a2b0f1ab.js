8c89e9a103599e9eb083e5d6e61c52680814e6da
Jorge Israelian <jorgeisraelian@g.ucla.edu>
2020-02-16 08:45:05 -0800

    Updates

---
+++ precache-manifest.dd260cf91efa4b96790bc8f7a2b0f1ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d65305c540c3705406fd237d889a583",
+    "url": "/einstein/index.html"
+  },
+  {
+    "revision": "1c7caac915a27253c255",
+    "url": "/einstein/static/js/2.e58447c9.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/einstein/static/js/2.e58447c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9955075e66e4ae4c1c8b",
+    "url": "/einstein/static/js/main.f9df781c.chunk.js"
+  },
+  {
+    "revision": "cfd950894711922d4ed1",
+    "url": "/einstein/static/js/runtime-main.1505ef3c.js"
+  }
+]);
